d88d6662670ef19d83efb0fcc39b20179708392f
Praveen Veerath <praveen.veerath@gmail.com>
2016-01-24 10:58:15 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Azure Web App\n');
+}).listen(port);
+
+console.log("Server listening on: http://localhost:%s", port);
