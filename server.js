4ed03ee882b5eceea96f196e9541d56d843cc22a
Praveen Veerath <praveen.veerath@gmail.com>
2016-01-25 10:09:09 -0800

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Azure Web App\n');
+  res.end('Hello from Azure Web App + PV\n');
 }).listen(port);
 
 console.log("Server listening on: http://localhost:%s", port);
